229573f209f198f5c4d676aa1375d7337bf330eb
carlton <carltonmbaja0@gmail.com>
2022-08-17 11:32:16 +0300

    Complete

---
+++ lib/dog.rb
@@ -0,0 +1,33 @@
+class Dog 
+    def bark
+        puts "Woof!"
+    end
+end
+
+fido = Dog.new
+fido.bark
+
+class Dog 
+    def sit
+        puts "The Dog is sitting"
+    end
+end
+fido = Dog.new
+fido.sit
+
+
+
+class Person
+    def talk
+        puts "Hello World!"
+    end
+
+    def walk
+        puts "The Person is walking"
+    end
+end
+ninja = Person.new
+ninja.talk
+
+ninja = Person.new
+ninja.walk
